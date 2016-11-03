10245b74519dd20825552c25f8e08376c3b205e4
aaronfrazer <aarfrazer@gmail.com>
2016-11-03 10:31:23 -0400

    Set up system email files. Set up email config variables.

--- application/config/email.php
+++ application/config/email.php
@@ -1,14 +1,47 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// PULL SETTINGS FROM CLIENT FOLDER
+require_once('client/clientemail.php');
+
+/*
+| -------------------------------------------------------------------------
+| EMAIL SETTINGS
+| -------------------------------------------------------------------------
+| This file lets you define global email preferences.
+|
+|
+*/
+
+//$config['protocol'] = 'smtp';
+//$config['wordwrap'] = TRUE;
+//$config['useragent'] = 'BZB';
+//$config['smtp_host'] = 'ssl://mail.onebzb.com';
+//$config['smtp_port'] = '465';
+//$config['smtp_user'] = 'update@onebzb.com';
+//$config['smtp_pass'] = '5BpqG$Z%GoHe';
+//$config['charset'] = 'utf-8';
+//$config['mailtype'] = 'html';
+//$config['newline'] = "\r\n";
+//
+//// $config['imap_host'] = 'server.iwatllc.com';
+//// $config['imap_host'] = '198.57.173.101';
+//$config['imap_host'] = '162.144.55.202';
+//$config['imap_user'] = 'iwat@onebzb.com';
+//$config['imap_pass'] = ']Q}K);BMOaBn';
+//// Non SSL Port
+//$config['imap_port'] = '143';
+//// SSL Port
+////$config['imap_port'] = '993';
+//$config['imap_mailbox'] = 'INBOX';
+//// Below is actually flags for imap.
+//$config['imap_path'] = '/imap/notls';
+//$config['imap_server_encoding'] = 'utf-8';
+//$config['imap_attachemnt_dir'] = './bbtmp/';
 
 
-$config['protocol'] = 'smtp';
-$config['wordwrap'] = TRUE;
-$config['useragent'] = 'BZB';
-$config['smtp_host'] = 'mail.onebzb.com';
-$config['smtp_port'] = '25';
-$config['smtp_user'] = 'matthew@iwatllc.com';
-$config['smtp_pass'] = '&Qqza6HT-A?q';
-$config['charset'] = 'utf-8';
-$config['mailtype'] = 'html';
-$config['newline'] = "\r\n";
+
+//{198.46.85.147:143/imap/user="iwat@onebzb.com"}INBOX
+
+
+/* End of file email.php */
+/* Location: ./system/application/config/email.php */
---
+++ client/clientemail.php
@@ -0,0 +1,38 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| EMAIL SETTINGS
+| -------------------------------------------------------------------------
+| This file lets you define global email preferences.
+|
+|
+*/
+
+$config['protocol'] = 'smtp';
+$config['wordwrap'] = TRUE;
+$config['useragent'] = 'BZB';
+$config['smtp_host'] = 'ssl://mail.onebzb.com';
+$config['smtp_port'] = '465';
+$config['smtp_user'] = 'update@onebzb.com';
+$config['smtp_pass'] = '5BpqG$Z%GoHe';
+$config['charset'] = 'utf-8';
+$config['mailtype'] = 'html';
+$config['newline'] = "\r\n";
+
+$config['imap_host'] = '162.144.55.202';
+$config['imap_user'] = 'iwat@onebzb.com';
+$config['imap_pass'] = ']Q}K);BMOaBn';
+// Non SSL Port
+$config['imap_port'] = '143';
+// SSL Port
+//$config['imap_port'] = '993';
+$config['imap_mailbox'] = 'INBOX';
+// Below is actually flags for imap.
+$config['imap_path'] = '/imap/notls';
+$config['imap_server_encoding'] = 'utf-8';
+$config['imap_attachemnt_dir'] = './bbtmp/';
+
+
+//{198.46.85.147:143/imap/user="iwat@onebzb.com"}INBOX
+
