95afb0359c73dc3aac8d217d2d7b7be53f4b902a
aaronfrazer <aarfrazer@gmail.com>
2016-11-03 10:32:18 -0400

    Added base_url() method to custom 404 page to correct external css/js files.

--- application/views/ezolp_404.blade.php
+++ application/views/ezolp_404.blade.php
@@ -13,17 +13,17 @@
 	
 	<!-- ================== BEGIN BASE CSS STYLE ================== -->
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-	<link href="assets/plugins/jquery-ui/themes/base/minified/jquery-ui.min.css" rel="stylesheet" />
-	<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-	<link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
-	<link href="assets/css/animate.min.css" rel="stylesheet" />
-	<link href="assets/css/style.min.css" rel="stylesheet" />
-	<link href="assets/css/style-responsive.min.css" rel="stylesheet" />
-	<link href="assets/css/theme/default.css" rel="stylesheet" id="theme" />
+	<link href="{{ base_url('assets/plugins/jquery-ui/themes/base/minified/jquery-ui.min.css') }}" rel="stylesheet" />
+	<link href="{{ base_url('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
+	<link href="{{ base_url('assets/plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" />
+	<link href="{{ base_url('assets/css/animate.min.css') }}" rel="stylesheet" />
+	<link href="{{ base_url('assets/css/style.min.css') }}" rel="stylesheet" />
+	<link href="{{ base_url('assets/css/style-responsive.min.css') }}" rel="stylesheet" />
+	<link href="{{ base_url('assets/css/theme/default.css') }}" rel="stylesheet" id="theme" />
 	<!-- ================== END BASE CSS STYLE ================== -->
 	
 	<!-- ================== BEGIN BASE JS ================== -->
-	<script src="assets/plugins/pace/pace.min.js"></script>
+	<script src="{{ base_url('assets/plugins/pace/pace.min.js') }}"></script>
 	<!-- ================== END BASE JS ================== -->
 </head>
 <body class="pace-top">
@@ -44,21 +44,21 @@
 	<!-- end page container -->
 
 	<!-- ================== BEGIN BASE JS ================== -->
-	<script src="assets/plugins/jquery/jquery-1.9.1.min.js"></script>
-	<script src="assets/plugins/jquery/jquery-migrate-1.1.0.min.js"></script>
-	<script src="assets/plugins/jquery-ui/ui/minified/jquery-ui.min.js"></script>
-	<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+	<script src="{{ base_url('assets/plugins/jquery/jquery-1.9.1.min.js') }}"></script>
+	<script src="{{ base_url('assets/plugins/jquery/jquery-migrate-1.1.0.min.js') }}"></script>
+	<script src="{{ base_url('assets/plugins/jquery-ui/ui/minified/jquery-ui.min.js') }}"></script>
+	<script src="{{ base_url('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
 	<!--[if lt IE 9]>
-		<script src="assets/crossbrowserjs/html5shiv.js"></script>
-		<script src="assets/crossbrowserjs/respond.min.js"></script>
-		<script src="assets/crossbrowserjs/excanvas.min.js"></script>
+		<script src="{{ base_url('assets/crossbrowserjs/html5shiv.js') }}"></script>
+		<script src="{{ base_url('assets/crossbrowserjs/respond.min.js') }}"></script>
+		<script src="{{ base_url('assets/crossbrowserjs/excanvas.min.js') }}"></script>
 	<![endif]-->
-	<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-	<script src="assets/plugins/jquery-cookie/jquery.cookie.js"></script>
+	<script src="{{ base_url('assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+	<script src="{{ base_url('assets/plugins/jquery-cookie/jquery.cookie.js') }}"></script>
 	<!-- ================== END BASE JS ================== -->
 
 	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
-	<script src="assets/js/apps.min.js"></script>
+	<script src="{{ base_url('assets/js/apps.min.js') }}"></script>
 	<!-- ================== END PAGE LEVEL JS ================== -->
 
 	<script>
